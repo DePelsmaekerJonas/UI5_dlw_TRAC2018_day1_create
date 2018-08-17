5bfa1ef9d17b0f94dd23fb6fd4b0db7b053e9dda
P1942920651 <jonas.depelsmaeker@delaware.pro>
2018-08-17 12:49:48 +0000

    initial commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.jonasdp.trac2018.createsalesorder.UI5_dlw_TRAC2018_day1_create.controller.App", {
+
+	});
+});
